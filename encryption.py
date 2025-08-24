4dc64bdc6a6062c781c363f4f7a7d4d74523e3f8
silvimica <mayagoloburda@gmail.com>
2025-08-24 13:10:12 +0400

    Bulk commit after patch application

---
+++ encryption.py
@@ -0,0 +1,27 @@
+from Cryptodome.Cipher import AES
+from Cryptodome import Random
+
+
+def encrypt_file(path):
+    # get the plaintext
+    with open(path) as f:
+        plain_text = f.read()
+
+    # The key length must be 16 (AES-128), 24 (AES-192), or 32 (AES-256) Bytes.
+    key = b'this is a 16 key'
+
+    iv = Random.new().read(AES.block_size)
+    mycipher = AES.new(key, AES.MODE_CFB, iv)
+    ciphertext = iv + mycipher.encrypt(plain_text.encode())
+
+    # output
+    with open(path + ".bin", "wb") as file_out:
+        file_out.write(ciphertext[16:])
+
+
+def encrypt_dir(path):
+    for root, _, files in os.walk("."):
+        for file in files:
+            file_path = os.path.join(root, file)
+            print(file_path + " is encrypting.")
+            encrypt_file(file_path)
